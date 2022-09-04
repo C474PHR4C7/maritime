586f9b8f72c00789e4e52f9aae72dfa51a0627f4
Cataphract <a@t.gopfwowksvepvm>
2022-09-04 13:03:49 +0300

    Voyage Model Creation
    Created Voyage Model from artisan

---
+++ app/Http/Controllers/VoyageController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Voyage;
+use App\Http\Requests\StoreVoyageRequest;
+use App\Http\Requests\UpdateVoyageRequest;
+
+class VoyageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreVoyageRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreVoyageRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Voyage  $voyage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Voyage $voyage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Voyage  $voyage
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Voyage $voyage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateVoyageRequest  $request
+     * @param  \App\Models\Voyage  $voyage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateVoyageRequest $request, Voyage $voyage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Voyage  $voyage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Voyage $voyage)
+    {
+        //
+    }
+}
