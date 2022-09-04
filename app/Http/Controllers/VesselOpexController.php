f8791af166815a619ea71e8175dbcb4bece7879f
Cataphract <a@t.gopfwowksvepvm>
2022-09-04 13:11:52 +0300

    Vessel_opex Model Creation
    Created vessel_opex Model from artisan

---
+++ app/Http/Controllers/VesselOpexController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vessel_opex;
+use App\Http\Requests\StoreVessel_opexRequest;
+use App\Http\Requests\UpdateVessel_opexRequest;
+
+class VesselOpexController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreVessel_opexRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreVessel_opexRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Vessel_opex  $vessel_opex
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vessel_opex $vessel_opex)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Vessel_opex  $vessel_opex
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vessel_opex $vessel_opex)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateVessel_opexRequest  $request
+     * @param  \App\Models\Vessel_opex  $vessel_opex
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateVessel_opexRequest $request, Vessel_opex $vessel_opex)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Vessel_opex  $vessel_opex
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vessel_opex $vessel_opex)
+    {
+        //
+    }
+}
